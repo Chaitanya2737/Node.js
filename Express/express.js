c78ef258f312724982926635735002ea2fc35fc6
Chaitanya <chaitanyasatarkar123@gmail.com>
2024-06-12 12:49:12 +0530

    routes

--- Express/express.js
+++ Express/express.js
@@ -1,23 +1,24 @@
-const express =  require("express")
+const express = require("express");
 
-const app =  express();
+const app = express();
 
 const user = require("./MOCK_DATA.json");
 
-
-app.get("/user" , (req ,  res)=> {
-    const html = `
+app.get("/user", (req, res) => {
+  const html = `
     <ul>
-    ${user.map((item)=> (item.first_name))}
+    ${user.map((item) => item.first_name)}
     </ul>
     
-    `
-    res.send(html)
-    })
-    
-    app.get("api/user" ,  (req , res)=>{
-        return res.json(user)
-    })
-app.listen(8000, () =>{
-    console.log("Server started")
-})
+    `;
+  res.send(html);
+});
+
+app.get("/api/user", (req, res) => {
+  return res.json(user);
+});
+
+
+app.listen(8000, () => {
+  console.log("Server started");
+});
